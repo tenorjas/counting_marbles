160e4dd218b24a74edb052c6d9042d6be62e29b5
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-15 11:47:53 -0400

    added functions GetTotalNumberOfMarbles, PrintInfo and RemoveAllGreenMarbles

--- Program.cs
+++ Program.cs
@@ -4,9 +4,35 @@ namespace counting_marbles
 {
     class Program
     {
+        public class MarbleBag
+        {
+            public int Reds {get; set;}
+            public int Greens {get; set;}
+            public int Blues {get; set;}
+
+            public int GetTotalNumberOfMarbles()
+            {
+                return Reds + Greens + Blues;
+            }
+
+            public void PrintInfo()
+            {
+                Console.WriteLine($"Your marble bag contains {Reds} red marbles.");
+                Console.WriteLine($"Your marble bag contains {Greens} green marbles.");
+                Console.WriteLine($"Your marble bag contains {Blues} blue marbles.");
+                Console.WriteLine($"Your marble bag contains a total of {GetTotalNumberOfMarbles()} marbles.");
+            }
+
+            public int RemoveAllGreenMarbles()
+            {
+                return Greens = 0;
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var marbleBag = new MarbleBag();
+
         }
     }
 }
